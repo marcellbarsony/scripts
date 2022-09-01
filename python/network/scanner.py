4e04ca8abd980b2a87f9385cfd17ba206b7e6a4c
marcellbarsony <barsonymarcell@gmail.com>
2022-09-01 00:59:49 +0000

    Modified Network Scanner

--- python/network/scanner.py
+++ python/network/scanner.py
@@ -9,10 +9,38 @@ with warnings.catch_warnings():
     import scapy.all as scapy
     #from scapy.all import *
 
-# Create ARP request
-def scan(ip):
-    arp_request = scapy.ARP()
-    print(arp_request.summary())
-    scapy.ls(scapy.ARP())
+ip="10.0.2.1/24"
+mac="ff:ff:ff:ff:ff:ff"
 
-scan("10.0.2.1/24")
+def scan(ip, mac):
+    # ARP layer
+    arp_request = scapy.ARP(pdst=ip)
+    # Ether layer
+    broadcast = scapy.Ether(dst=mac)
+
+    # Ethernet frame
+    arp_request_broadcast = broadcast/arp_request
+
+    # Send & Capture packet
+    # Requires 'sudo -E' to preserve environment
+    # https://python-forum.io/thread-36293.html
+    #ans, unans = scapy.srp(arp_request_broadcast, timeout=1)
+    ans = scapy.srp(arp_request_broadcast, timeout=1)[0]
+    for element in ans:
+        print(element)
+        print("-----")
+    #print(ans.summary())
+
+    # Show
+    #arp_request_broadcast.show()
+    
+    # Summary
+    #print(arp_request.summary())
+    #print(broadcast.summary())
+    #print(arp_request_broadcast.summary())
+    
+    # Get info
+    #scapy.ls(scapy.ARP())
+    #scapy.ls(scapy.Ether())
+
+scan(ip, mac)
