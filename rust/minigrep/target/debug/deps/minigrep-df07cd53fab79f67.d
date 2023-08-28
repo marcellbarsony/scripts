fed5f117015f8826acf9cd793833ee199ed19ec5
marcellbarsony <barsonymarcell@protonmail.com>
2023-08-29 00:44:24 +0200

    Removed Rust/target

--- rust/minigrep/target/debug/deps/minigrep-df07cd53fab79f67.d
+++
@@ -1,5 +0,0 @@
-/home/marci/.local/git/scripts/rust/minigrep/target/debug/deps/minigrep-df07cd53fab79f67.rmeta: src/main.rs
-
-/home/marci/.local/git/scripts/rust/minigrep/target/debug/deps/minigrep-df07cd53fab79f67.d: src/main.rs
-
-src/main.rs:
