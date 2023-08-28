fc349bb9596779cd062089ffff328a91b44b16f7
marcellbarsony <barsonymarcell@protonmail.com>
2023-08-29 00:42:45 +0200

    Added Rust

---
+++ rust/minigrep/target/debug/deps/minigrep-f26b30b36e9d8b8c.d
@@ -0,0 +1,5 @@
+/home/marci/.local/git/scripts/rust/minigrep/target/debug/deps/minigrep-f26b30b36e9d8b8c: src/main.rs
+
+/home/marci/.local/git/scripts/rust/minigrep/target/debug/deps/minigrep-f26b30b36e9d8b8c.d: src/main.rs
+
+src/main.rs:
